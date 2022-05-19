1357af8d7d62c3137e870c58b6ef3f72eceb2f1d
Fredi-B <fredibechtoldt@outlook.com>
2022-05-19 15:10:41 +0200

    added comments

--- print_macros.h
+++ print_macros.h
@@ -1,5 +1,9 @@
 
 /* ---------------------- Defines for debugging ---------------------------- */
+/* print the name of the variable followed by it's value: */
+/* print a string */
 # define dsprintf(expr) printf("\n" #expr ":\n|%s|\n", expr)
+/* print an integer */
 # define diprintf(expr) printf("\n" #expr ":\n|%d|\n", expr)
+/* write an expression. fd, quotes and length is taken care of */
 # define dwrite(expr) write(2, "\n" #expr "\n", strlen(#expr) + 2)
