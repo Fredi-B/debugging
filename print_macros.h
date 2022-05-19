cde6dd8828d0408fe242adbd3210e9ecbf884741
Fredi-B <fredibechtoldt@outlook.com>
2022-05-19 15:03:13 +0200

    added some print makros

---
+++ print_macros.h
@@ -0,0 +1,5 @@
+
+/* ---------------------- Defines for debugging ---------------------------- */
+# define dsprintf(expr) printf("\n" #expr ":\n|%s|\n", expr)
+# define diprintf(expr) printf("\n" #expr ":\n|%d|\n", expr)
+# define dwrite(expr) write(2, "\n" #expr "\n", strlen(#expr) + 2)
